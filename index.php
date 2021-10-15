81794f9f1ea708ba8947deff2864b768d3d5ffd6
ardiyantop8 <ardiyantop8@gmail.com>
2021-10-15 10:37:12 +0700

    menambahakan note untuk pengertian line

--- index.php
+++ index.php
@@ -7,12 +7,22 @@
     <title>Create QRCode</title>
 </head>
 <body>
-    <?php 
+    <?php
+    /* INISIASI UNTUK ISI QRCODE */ 
         $kode = "ardiqrcode"."b412ay";
+    /* TUTUP INISIASI UNTUK ISI QRCODE */
+        
+    /*PEMANGGILAN LIBRARY QRCODE */
         require_once('assets/phpqrcode/qrlib.php');
+    /*TUTUP PEMANGGILAN LIBRARY QRCODE */
+
+     /*FORMAT QRCODE DAN PENAMAAN QRCODE PNG DAN UKURANYA */
         QRcode::png("$kode", "kode"."1".".png","M",6,6);
+     /*TUTUP FORMAT QRCODE DAN PENAMAAN QRCODE PNG DAN UKURANYA */
     ?>
     <h1>
+
+        <!-- Menampilkan qrcode -->
         <img src="kode1.png" alt="">
     </h1>
 </body>
